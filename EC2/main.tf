0b87f69334be20868620d61b806f6df68cfb2dd9
KhomotsoMashupye <motsomash2242@gmail.com>
2026-03-25 07:49:31 +0200

    Add user data to the ec2 instance

--- EC2/main.tf
+++ EC2/main.tf
@@ -184,3 +184,15 @@ resource "aws_instance" "standalone_ec2" {
     Name = "placeholder-standalone-ec2"
   }
 }
+ # USER DATA(used to avoid manually sshing into an instance)
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "<h1>EC2 Instance $(hostname)</h1>" > /var/www/html/index.html
+              EOF
+
+
